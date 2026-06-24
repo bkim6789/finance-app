9530d7faf788d6ac28d131f5bfa52f74ba8ea397
bkim6789 <briangkim@gmail.com>
2026-06-24 10:58:16 -0700

    formatted username

--- src/app/toolbar/toolbar.tsx
+++ src/app/toolbar/toolbar.tsx
@@ -1,22 +1,32 @@
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../../store'
+import {
+  selectFormattedUsername,
+  selectUserIsAttemptingToLogIn,
+  selectUserIsLoggedIn,
+  selectUserLoginError,
+} from '../../shared/user/userSlice'
 
 export const Toolbar = () => {
-  const [loginUsername, setLoginUsername] = useState('');
-  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
-  const loginError = useSelector((state: RootState) => state.user.loginError);
+  const [inputUsername, setInputUsername] = useState('');
+  const username = useSelector((state: RootState) => selectFormattedUsername(state, true));
+  const isLoggedIn = useSelector(selectUserIsLoggedIn);
+  const isAttemptingToLogIn = useSelector(selectUserIsAttemptingToLogIn);
+  const loginError = useSelector(selectUserLoginError);
 
   const handleLogIn = () => {
-    console.warn('@bgk loging:', loginUsername);
+    console.warn('@bgk loging:', inputUsername);
   }
 
   return (
     <div>
+      <div>username: {username}</div>
       <div>isLoggedIn: {String(isLoggedIn)}</div>
+      <div>isAttemptingToLogIn: {String(isAttemptingToLogIn)}</div>
       <div>error: {loginError}</div>
       
-      username: <input type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)} />
+      username input: <input type="text" value={inputUsername} onChange={(e) => setInputUsername(e.target.value)} />
       <button onClick={handleLogIn}>Log In</button>
     </div>
   );
--- src/shared/user/userSlice.ts
+++ src/shared/user/userSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface UserData {
   username: string;
@@ -11,7 +11,7 @@ export const userSlice = createSlice({
   name: 'user',
   initialState: () => {
     return {
-      username: 'init',
+      username: 'iNiT',
       isLoggedIn: false,
       isAttemptingToLogIn: false,
       loginError: 'test error!',
@@ -26,3 +26,25 @@ export const userSlice = createSlice({
     },
   },
 })
+
+export const selectUsername = (state: { user: UserData }) =>
+  state.user.username;
+
+// @bgk the selectors for selectUsername need to all have shouldCapitalize arg
+const selectShouldCapitalize = (_state: { user: UserData }, shouldCapitalize: boolean) =>
+  shouldCapitalize;
+
+export const selectFormattedUsername = createSelector(
+  [selectUsername, selectShouldCapitalize],
+  (username, shouldCapitalize) =>
+    shouldCapitalize ? username.toUpperCase() : username.toLowerCase(),
+);
+
+export const selectUserIsLoggedIn = (state: { user: UserData }) =>
+  state.user.isLoggedIn;
+
+export const selectUserIsAttemptingToLogIn = (state: { user: UserData }) =>
+  state.user.isAttemptingToLogIn;
+
+export const selectUserLoginError = (state: { user: UserData }) =>
+  state.user.loginError;
